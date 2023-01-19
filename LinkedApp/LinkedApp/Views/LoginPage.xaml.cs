4dd2f95a8f96e9784e5267c1ef9b1bf11e0e751d
Sebastian Andrei <sebastian.andrei@totalsoft.ro>
2023-01-19 11:27:38 +0200

    v0.0.01

---
+++ LinkedApp/LinkedApp/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using LinkedApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LinkedApp.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
